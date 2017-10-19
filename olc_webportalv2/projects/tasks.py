00e9c61dc42af60f87ccaf18acda740afdd757dd
Forest Dussault <forest.dussault@inspection.gc.ca>
2017-10-19 09:01:26 -0400

    GenesipprResults model update bug fix

--- olc_webportalv2/projects/tasks.py
+++ olc_webportalv2/projects/tasks.py
@@ -8,10 +8,12 @@ import csv
 @background(schedule=5)
 def run_genesippr(file_path, proj_pk):
 
+    # Set genesippr status
     Project.objects.filter(id=proj_pk).update(genesippr_status="Processing...")
 
     print('\nrun_genesippr() called successfully for project ID {}'.format(proj_pk))
 
+    # Run Genesippr
     cmd = 'docker exec ' \
           'genesipprv2 ' \
           'python3 ' \
@@ -28,54 +30,31 @@ def run_genesippr(file_path, proj_pk):
     genesippr_result_path = glob.glob('olc_webportalv2/media/{}/reports/genesippr.csv'.format(file_path))
     genesippr_result_path = genesippr_result_path[0]
 
-    print('Attempting to read ' + genesippr_result_path)
-    read_genesippr_results(genesippr_result_path, proj_pk)
+    print('\nAttempting to read ' + genesippr_result_path)
+    try:
+        read_genesippr_results(genesippr_result_path, proj_pk)
+        Project.objects.filter(pk=proj_pk).update(genesippr_status="Complete")
+        print('\nReading genesippr results complete.')
+    except:
+        print('\nReading genesippr results failed.')
 
-    Project.objects.filter(pk=proj_pk).update(genesippr_status="Complete")
 
-
-def add_genesippr_csv_result(strain, genus, proj_pk):
+def add_genesippr_csv_result(proj_pk, strain, genus,):
     """
     Function to retrieve a specific project ID, then update the GenesipprResults model with according data from
     the genesippr.csv report created.
     """
-    c = GenesipprResults.objects.get_or_create(project=Project.objects.get(id=proj_pk),
-                                               strain=strain,
-                                               genus=genus)
+    c = GenesipprResults.objects.filter(project=Project.objects.get(id=proj_pk)).update(strain=strain,
+                                                                                        genus=genus,)
     return c
 
 
 def read_genesippr_results(genesippr_result_path, proj_pk):
     with open(genesippr_result_path, newline='', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=',')
-        header = next(reader)
+        # header = next(reader)
         for row in reader:
             add_genesippr_csv_result(proj_pk=proj_pk,
                                      strain=str(row[0]),
                                      genus=str(row[1]),
                                      )
-            # print(row)
-            # _, created = GenesipprResults.objects.get_or_create(
-            #     strain=row[0],
-            #     genus=row[1],
-            #     # O26=row[3],
-            #     # O45=row[4],
-            #     # O103=row[5],
-            #     # O111=row[6],
-            #     # O121=row[7],
-            #     # O145=row[8],
-            #     # O157=row[9],
-            #     # VT1=row[10],
-            #     # VT2=row[11],
-            #     # VT2f=row[12],
-            #     # uidA=row[13],
-            #     # eae=row[14],
-            #     # eae_1=row[15],
-            #     # IGS=row[16],
-            #     # hylA=row[17],
-            #     # inlJ=row[18],
-            #     # invA=row[19],
-            #     # stn=row[20],
-            # )
-    # except:
-    #     print('read_genesippr_results() failed catastrophically')
