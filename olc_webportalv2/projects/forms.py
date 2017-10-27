f68f2b6089d97183a3d6390b1c67965968237e96
Forest Dussault <forest.dussault@inspection.gc.ca>
2017-10-27 14:52:29 -0400

    Added description field

--- olc_webportalv2/projects/forms.py
+++ olc_webportalv2/projects/forms.py
@@ -15,12 +15,13 @@ class ProjectForm(BootstrapModelForm):
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
         self.fields['project_title'].widget.attrs.update({'placeholder': 'Enter a project title'})
+        self.fields['description'].widget.attrs.update({'placeholder': 'Enter a brief project description (optional)'})
         self.fields['requested_jobs'].widget.attrs.update({'class': 'btn btn-success'})
-
 
     class Meta:
         model = Project
         fields = ('project_title',
+                  'description',
                   'file_R1',
                   'file_R2',
                   'requested_jobs')
