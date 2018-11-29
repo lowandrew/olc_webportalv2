61ea4eec09b4f10a0097a600adeb56079a245d0e
Andrew Low <andrew.low@canada.ca>
2018-11-29 15:17:08 -0500

    GeneSeekr form testing done for now

--- olc_webportalv2/geneseekr/tests/test_forms.py
+++ olc_webportalv2/geneseekr/tests/test_forms.py
@@ -2,16 +2,12 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from olc_webportalv2.geneseekr.forms import ParsnpForm, GeneSeekrForm
 from olc_webportalv2.metadata.models import SequenceData
-from olc_webportalv2.users.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
-class FormTest(TestCase):
+class GeneSeekrFormTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user = User.objects.create(username='TestUser')
-        user.set_password('password')
-        user.save()
         sequence_data = SequenceData.objects.create(seqid='2015-SEQ-0711',
                                                     quality='Pass',
                                                     genus='Listeria')
@@ -56,3 +52,57 @@ class FormTest(TestCase):
         with open('olc_webportalv2/geneseekr/tests/bad_fasta.fasta', 'rb') as upload_file:
             form = GeneSeekrForm({'seqids': '2015-SEQ-0711 2015-SEQ-0712'}, {'query_file': SimpleUploadedFile(upload_file.name, upload_file.read())})
             self.assertFalse(form.is_valid())
+
+    def test_invalid_form_no_sequences_in_genus(self):
+        form = GeneSeekrForm({
+            'genus': 'TotallyFakeGenus',
+            'query_sequence': '>fasta_name\nATCGACTGACTAGTCA'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_form_no_sequences_in_exclude_genus(self):
+        form = GeneSeekrForm({
+            'genus': 'Listeria',
+            'query_sequence': '>fasta_name\nATCGACTGACTAGTCA',
+            'everything_but': True,
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_valid_form_exclude_genus(self):
+        form = GeneSeekrForm({
+            'genus': 'Salmonella',
+            'query_sequence': '>fasta_name\nATCGACTGACTAGTCA',
+            'everything_but': True,
+        })
+        self.assertTrue(form.is_valid())
+
+
+class ParsnpFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        sequence_data = SequenceData.objects.create(seqid='2015-SEQ-0711',
+                                                    quality='Pass',
+                                                    genus='Listeria')
+        sequence_data.save()
+        sequence_data = SequenceData.objects.create(seqid='2015-SEQ-0712',
+                                                    quality='Pass',
+                                                    genus='Listeria')
+        sequence_data.save()
+
+    def test_valid_form(self):
+        form = ParsnpForm({
+            'seqids': '2015-SEQ-0711 2015-SEQ-0712'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form_wrong_seqid_regex(self):
+        form = ParsnpForm({
+            'seqids': '22015-SEQ-0711 2015-SEQ-0712'
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_form_wrong_seqid_does_not_exist(self):
+        form = ParsnpForm({
+            'seqids': '2222-SEQ-0711 2015-SEQ-0712'
+        })
+        self.assertFalse(form.is_valid())
